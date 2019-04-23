d507ab8f9a5022d81ea19840300f49234b72d196
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-23 14:22:04 -0400

    Initial Commit

---
+++ contracts/MetaNFT.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-eth/contracts/token/ERC721/StandaloneERC721.sol";
+import "zos-lib/contracts/Initializable.sol";
+
+contract MetaNFT is Initializable, StandaloneERC721 {
+    function initialize(string memory name, string memory symbol, address[] memory minters, address[] memory pausers) public initializer {
+    StandaloneERC721.initialize(name, symbol, minters, pausers);
+    
+    }
+}
