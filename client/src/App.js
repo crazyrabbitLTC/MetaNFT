2d6ca5778a4c8d418bcc8a7be08bc55eec6bb77b
Dennison Bertram <dennison@dennisonbertram.com>
2019-04-23 15:29:00 -0400

    find out that hooks are installed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import getWeb3, { getGanacheWeb3 } from "./utils/getWeb3";
 import Web3Info from "./components/Web3Info/index.js";
 import { Loader } from 'rimble-ui';
@@ -6,92 +6,94 @@ import { zeppelinSolidityHotLoaderOptions } from '../config/webpack';
 
 import styles from './App.module.scss';
 
-class App extends Component {
-  state = {
+function App() {
+  
+  const initialState = {
     storageValue: 0,
     web3: null,
     accounts: null,
     contract: null,
     token: null,
-    route: window.location.pathname.replace("/","")
+    route: window.location.pathname.replace("/",""),
+    pollInterval: null,
+    ganacheProvider: null,
   };
 
-  getGanacheAddresses = async () => {
-    if (!this.ganacheProvider) {
-      this.ganacheProvider = getGanacheWeb3();
-    }
-    if (this.ganacheProvider) {
-      return await this.ganacheProvider.eth.getAccounts();
-    }
-    return [];
-  }
-
-  componentDidMount = async () => {
-    try {
-      const isProd = process.env.NODE_ENV === 'production';
-      if (!isProd) {
-        // Get network provider and web3 instance.
-        const web3 = await getWeb3();
-        const ganacheAccounts = await this.getGanacheAddresses();
-        // Use web3 to get the user's accounts.
-        const accounts = await web3.eth.getAccounts();
-        // Get the contract instance.
-        const networkId = await web3.eth.net.getId();
-        const isMetaMask = web3.currentProvider.isMetaMask;
-        let balance = accounts.length > 0 ? await web3.eth.getBalance(accounts[0]): web3.utils.toWei('0');
-        balance = web3.utils.fromWei(balance, 'ether');
-
-        let ERC721 = {};
-        try {
-          ERC721 = require("../../contracts/MetaNFT.sol");
-          //console.log(ERC721);
-        } catch (e) {
-          console.log(e);
-        }
-
-        let tokenSupply = 
-        this.setState({ web3, ganacheAccounts, accounts, balance, networkId, isMetaMask });
-      }
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
+  // const [state, setState] = useState(initialState);
 
 
-  };
+  // const getGanacheAddresses = async () => {
+  //   if (!state.ganacheProvider) {
+  //     state.ganacheProvider = getGanacheWeb3();
+  //   }
+  //   if (state.ganacheProvider) {
+  //     return await state.ganacheProvider.eth.getAccounts();
+  //   }
+  //   return [];
+  // }
 
-  componentWillUnmount() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-  }
+  // useEffect(() => {
 
-  renderLoader() {
-    return (
-      <div className={styles.loader}>
-        <Loader size="80px" color="red" />
-        <h3> Loading Web3, accounts, and contract...</h3>
-        <p> Unlock your metamask </p>
-      </div>
-    );
-  }
+  //   const loadProviderData = async() => {
 
-  render() {
-    if (!this.state.web3) {
-      return this.renderLoader();
-    }
-    return (
-      <div className={styles.App}>
-        <h1>Good to Go!</h1>
-        <p>Zepkit has created your app.</p>
-        <h2>See your web3 info below:</h2>
-        <Web3Info {...this.state} />
-      </div>
-    );
-  }
+  //     const isProd = process.env.NODE_ENV === 'production';
+  //     const web3 = await getWeb3();
+  //     const ganacheAccounts = await getGanacheAddresses();
+
+  //     // Use web3 to get the user's accounts.
+  //     const accounts = await web3.eth.getAccounts();
+
+  //     // Get the contract instance.
+  //     let ERC721 = {};
+  //     const networkId = await web3.eth.net.getId();
+  //     const isMetaMask = web3.currentProvider.isMetaMask;
+  //     let balance = accounts.length > 0 ? await web3.eth.getBalance(accounts[0]): web3.utils.toWei('0');
+  //     balance = web3.utils.fromWei(balance, 'ether');
+  //     ERC721 = require("../../contracts/MetaNFT.sol");
+  //     setState({ web3, ganacheAccounts, accounts, balance, networkId, isMetaMask });
+  //   }
+
+  //   try {
+  //     loadProviderData();
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+
+  //   if(state.pollInterval){
+  //     return clearInterval(state.pollInterval)
+  //   }
+
+  // },[])
+
+  // renderLoading = () =>  {
+  //   return (
+  //     <div className={styles.loader}>
+  //       <Loader size="80px" color="red" />
+  //       <h3> Loading Web3, accounts, and contract...</h3>
+  //       <p> Unlock your metamask </p>
+  //     </div>
+  //   );
+  // }
+
+  // renderLoaded = () =>  {
+  //   return (
+  //     <div className={styles.App}>
+  //       <h1>Good to Go!</h1>
+  //       <p>Zepkit has created your app.</p>
+  //       <h2>See your web3 info below:</h2>
+  //       <Web3Info {...this.state} />
+  //     </div>
+  //   );
+  // }
+
+  return (
+    <div>
+      <p>You clicked times</p>
+      <p>Currently using React {React.version}</p> : null
+    </div>
+  );
+
 }
 
 export default App;
+
